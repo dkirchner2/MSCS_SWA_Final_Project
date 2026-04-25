c89e6c20b6fd0e8b648f316752aa8c0f1b43b01e
dk4267 <dkirchner4267@gmail.com>
2026-04-24 22:55:33 -0500

    throw stuff at the wall

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
 from flask import Flask, request, render_template, jsonify
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'collector')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'analyzer')))
-import collector.data_fetcher as fetcher
-import analyzer.data_analyzer as analyzer
+from datetime import datetime
+from collector.data_fetcher import initialize_data, get_location_data, fetch_daily_weather, get_city_weather_data
+from analyzer.data_analyzer import get_monthly_weather_data
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    fetcher.initialize_data()
-    location_data = fetcher.get_location_data()
+    initialize_data()
+    location_data = get_location_data()
     location_dict = [dict(row) for row in location_data] 
     return render_template('index.html', locations=location_dict)
 
@@ -20,9 +17,9 @@ def main():
 @app.route("/yesterday", methods=["GET"])
 def pull_yesterdays_weather():
     print('woo')
-    fetcher.initialize_data()
+    initialize_data()
     yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
-    fetcher.fetch_daily_weather(yesterday_date)
+    fetch_daily_weather(yesterday_date)
 
 
 @app.route("/cities", methods=["POST"])
@@ -31,9 +28,10 @@ def get_city_weather():
     data = request.get_json()
     city_data_results = []
     for city_idx in data.get('cities'):
-        city_data_df = fetcher.get_city_weather_data(city_idx)
-        print(city_data_df)
-        result = analyzer.get_monthly_weather_data(city_data_df)
+        city_data_df = get_city_weather_data(city_idx)
+        result = get_monthly_weather_data(city_data_df)
         city_data_results.append(result)
     return jsonify(city_data_results)
 
+if __name__ == "__main__":
+    app.run()
